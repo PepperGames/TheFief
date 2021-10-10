ad61ffd5bf9847c4a464c76171b0946e8f6e03bd
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2021-10-10 11:46:09 +0300

    added building houses 1*1 2*2

--- Assets/Core/Scripts/Building/GameManager.cs
+++ Assets/Core/Scripts/Building/GameManager.cs
@@ -10,13 +10,48 @@ public class GameManager : MonoBehaviour
     public RoadManager roadManager;
     public InputManager inputManager;
 
+    public StructureManager structureManager;
+
+    public TestUIController uiController;
     private void Start()
     {
-        inputManager.OnMouseClick += roadManager.PlaceRoad; ;
-        inputManager.OnMouseHold += roadManager.PlaceRoad; ;
+        uiController.OnRoadPlacement += RoadPlacementHandler;
+        uiController.OnHousePlacement += HousePlacementHandler;
+        uiController.OnSpecialPlacement += SpecialPlacementHandler;
+        uiController.OnBigStructurePlacement += BigStructurePlacementHandler;
+    }
 
+    private void BigStructurePlacementHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.PlaceBigStructure;
+    }
+
+    private void SpecialPlacementHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.PlaceSpecial;
+    }
+
+    private void HousePlacementHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.PlaceHouse;
+    }
+
+    private void RoadPlacementHandler()
+    {
+        ClearInputActions();
+
+        inputManager.OnMouseClick += roadManager.PlaceRoad;
+        inputManager.OnMouseHold += roadManager.PlaceRoad;
         inputManager.OnMouseUp += roadManager.FinishPlacingRoad;
-
+    }
+    private void ClearInputActions()
+    {
+        inputManager.OnMouseClick = null;
+        inputManager.OnMouseHold = null;
+        inputManager.OnMouseUp = null;
     }
 
     private void Update()
