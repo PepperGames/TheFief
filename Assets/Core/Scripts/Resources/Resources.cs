6799e9c8d95d5cab5b0bb4e67333cb1de7fa2ec6
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2021-11-07 21:02:34 +0200

    add resources class

--- Assets/Core/Scripts/Resources/Resources.cs
+++ Assets/Core/Scripts/Resources/Resources.cs
@@ -2,17 +2,111 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resources : MonoBehaviour
+public struct Resources
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float money, food, wood, stone, metal;
+
+    #region PROPERTIES
+
+    public float Money
     {
-        
+        get => money;
+        set
+        {
+            if (value >= 0)
+            {
+                money = value;
+            }
+            else
+            {
+                Debug.Log("negative value " + value);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public float Food
     {
-        
+        get => food;
+        set
+        {
+            if (value >= 0)
+            {
+                food = value;
+            }
+            else
+            {
+                Debug.Log("negative value " + value);
+            }
+        }
     }
+
+    public float Wood
+    {
+        get => wood;
+        set
+        {
+            if (value >= 0)
+            {
+                wood = value;
+            }
+            else
+            {
+                Debug.Log("negative value " + value);
+            }
+        }
+    }
+
+    public float Stone
+    {
+        get => stone;
+        set
+        {
+            if (value >= 0)
+            {
+                stone = value;
+            }
+            else
+            {
+                Debug.Log("negative value " + value);
+            }
+        }
+    }
+
+    public float Metal
+    {
+        get => metal;
+        set
+        {
+            if (value >= 0)
+            {
+                metal = value;
+            }
+            else
+            {
+                Debug.Log("negative value " + value);
+            }
+        }
+    }
+
+    #endregion
+
+    public Resources(float money, float food, float wood, float stone, float metal)
+    {
+        this.money = money;
+        this.food = food;
+        this.wood = wood;
+        this.stone = stone;
+        this.metal = metal;
+    }
+
+    public static Resources operator +(Resources a, Resources b)
+    {
+        return new Resources(a.Money + b.Money, a.Food + b.Food, a.Wood + b.Wood, a.Stone + b.Stone, a.Metal + b.Metal);
+    }
+
+    public static Resources operator -(Resources a, Resources b)
+    {
+        return new Resources(a.Money - b.Money, a.Food - b.Food, a.Wood - b.Wood, a.Stone - b.Stone, a.Metal - b.Metal);
+    }
+
 }
