97b8c92b5132bc340509a86bc975cff5b04e3b0c
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2022-06-07 17:53:51 +0300

    StartMenu

--- Assets/Core/1. Scripts/UI/Menu/StartMenu/StartMenu.cs
+++ Assets/Core/1. Scripts/UI/Menu/StartMenu/StartMenu.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _startMenu;
+    [SerializeField] private GameObject _choiceOfRuler;
+
+    [SerializeField] private Button _startGameButton;
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _exitButton;
+
+    private void Start()
     {
-        
+        _startMenu.SetActive(true);
+        _choiceOfRuler.SetActive(false);
+
+        _startGameButton.onClick.AddListener(ShowChoiceOfRuler);
+        _playButton.onClick.AddListener(Play);
+        _exitButton.onClick.AddListener(Exit);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowChoiceOfRuler()
     {
-        
+        _startMenu.SetActive(false);
+        _choiceOfRuler.SetActive(true);
+    }
+
+    public void Play()
+    {
+        string sceneName = "MainScene";
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
